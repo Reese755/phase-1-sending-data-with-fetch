7219ae93225a2717eeebf917bdb4cdb9ccb576bc
Reese Marshall <reese.amarshall@gmail.com>
2022-12-19 10:38:32 -0500

    commit tree fce8528e87ec177b6bed326860739400c75895d1

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+
+const form =document.querySelector("#form-to-submit")
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const form = event.target
+})
+
+ fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json'
+        },
+            body: JSON.stringify({
+                'name': name,
+                'email': email
+                
+            })
+
+    })        .then(response => response.json())
+    .then(submitData => document.body.append(submitData.id))
+    .catch(error => document.body.append(error))
